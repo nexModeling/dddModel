93713b358057becce7cafa7e9cfc884442d974d0
jeanmarielepioufle <jm@nexmodeling.com>
2016-10-20 00:34:48 +0200

    update datasets documentation

---
+++ R/paramCalibrated.R
@@ -0,0 +1,13 @@
+#' Set of calibrated parameters from daily discharge at Narsjo
+#'
+#' Set of calibrated parameters from daily discharge at Narsjo.
+#' Some parameters might be erroneous.
+#' The set of parameters is not to be taken as robust but as an example
+#'
+#' @docType data
+#' @name paramCalibrated
+#' @format A R file
+#' @keywords datasets
+#' @references the Norwegian Water Resources and Energy Directorate (NVE)
+#' @source \href{http://www.nve.no}{NVE}
+NULL
---
+++ R/paramGIS.R
@@ -0,0 +1,13 @@
+#' Set of GIS parameters related to the catchement at Narsjo
+#'
+#' Set of GIS parameters related to the catchement at Narsjo
+#' Some parameters might be erroneous.
+#' The set of parameters is not to be taken as robust but as an example
+#'
+#' @docType data
+#' @name paramGIS
+#' @format A R file
+#' @keywords datasets
+#' @references the Norwegian Water Resources and Energy Directorate (NVE)
+#' @source \href{http://www.nve.no}{NVE}
+NULL
