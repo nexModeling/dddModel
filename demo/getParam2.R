61ff85b6e1b1e6d9b5fd82e2f153d3c1a567aaf9
jeanmarielepioufle <jm@nexmodeling.com>
2016-10-25 10:01:21 +0200

    deleted outdated code

--- demo/getParam2.R
+++ demo/getParam2.R
@@ -5,12 +5,3 @@ filename <- "paramNVE.txt"
 
 inputParam <- getParam(method="processedNVE",path=path, filename=filename,SAVE=TRUE,pathResults="~/")
 str(inputParam)
-
-
-path <- "C:/Users/jm/Google Drive/nexModeling/2. Product/R/Rlibraries_ddd/DATA fromTS/"
-
-#setwd(path)
-filename <- "best_par_50.13_24h.txt"
-
-inputParam <- getParam(method="processedNVE",path=path, filename=filename,SAVE=TRUE,pathResults="~/")
-str(inputParam)
